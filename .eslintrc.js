a948d4675a3c77f25efc1b863da6e81207161f93
Sean O'Grady <sean@seanogrady.me>
2021-07-17 03:03:24 +0100

    Setup

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'airbnb-typescript/base',
+    '@vue/typescript/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    project: 'tsconfig.eslint.json',
+  },
+  rules: {
+    'import/extensions': [
+      'error',
+      'always',
+      {
+        js: 'never',
+        mjs: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'no-param-reassign': [
+      'error',
+      {
+        props: true,
+        ignorePropertyModificationsFor: [
+          'state', // for vuex state
+          'acc', // for reduce accumulators
+          'e', // for e.returnvalue
+        ],
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      node: {},
+      typescript: {},
+    },
+  },
+};
